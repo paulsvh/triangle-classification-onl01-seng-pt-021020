bddb8da910b876f5b83bcf52a57ea6d02a8506f3
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-15 21:28:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind #returns type of triangle (equilateral, isosceles, or scalene). Raises custom error TriangleError if triangle is invalid
+  end
+
+  class TriangleError < StandardError
+   def message
+     "The supplied side lengths do not form a triangle!"
+   end
+ end
+
 end
+
+
+
+#TRIANGLE VALIDITY:
+#The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality.
+#Further, each side must be larger than 0.
