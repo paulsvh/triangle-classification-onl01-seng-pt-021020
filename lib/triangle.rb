0b9b4a2433c0a71620a901680c51db40cdaa22d8
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-15 21:30:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,27 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
 
+  sides = []
+
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
+    sides << side1
+    sides << side2
+    sides << side3
   end
 
   def kind #returns type of triangle (equilateral, isosceles, or scalene). Raises custom error TriangleError if triangle is invalid
+    if person.class != Person
+     begin
+       raise TriangleError
+     rescue TriangleError => error
+         puts error.message
+     end
+   else
+
+   end
   end
 
   class TriangleError < StandardError
